690940ae087e77c5d3d00bf14c42819545812712
Kundrapu Naresh <129973630+nareshkundrapu123@users.noreply.github.com>
2025-09-06 23:43:26 +0530

    multiple routhandling

--- src/App.js
+++ src/App.js
@@ -1,37 +1,56 @@
 const express = require('express');
 const app = express();
 
-app.get("/user/:userId",(req,res)=>{
-  console.log(req.params);
-  res.send({firstName: "Naresh", lastName: "Kundrapu"});
-});
+// app.get("/user/:userId",(req,res)=>{
+//   console.log(req.params);
+//   res.send({firstName: "Naresh", lastName: "Kundrapu"});
+// });
+
+app.use("/user",(req,res,next)=>{
+  console.log("Handling the router user");
+  //res.send("Response!");
+  next();  
+},
+(req,res,next)=>{
+  console.log("Handling the router user 2");
+  // res.send("2 Response!");
+  next();                                                 
+},
+(req,res,next)=>{
+  console.log("Handling the router user 3");
+  // res.send("3 Response!");                                 
+  next();
+} ,(req,res,next)=>{
+  console.log("Handling the router user 4");
+   res.send("4 Response!");                                 
+} );
 
 // Regex
-app.get(/ab/,(req,res)=>{
-  res.send({firstName: "Naresh", lastName: "Kundrapu"}                  );
-});
+// app.get(/ab/,(req,res)=>{
+//   res.send({firstName: "Naresh", lastName: "Kundrapu"}                  );
+// });
 
-app.post("/user",(req,res)=>{
-  res.send("Data is saved successfully"); 
-});
-
-
-app.delete("/user",(req,res)=>{
-  res.send("data is deleted successfully"); 
-});
-
-app.use("/hello/guru",(req,res)=>{
-  res.send('Hai guru!');
-});
-
-app.use("/hello",(req,res)=>{
-  res.send('Hai Naresh!');
-});
+// app.post("/user",(req,res)=>{
+//   res.send("Data is saved successfully"); 
+// });
 
 
-app.use("/test",(req,res)=>{
-  res.send('Hai Test!');
-});
+// app.delete("/user",(req,res)=>{
+//   res.send("data is deleted successfully"); 
+// });
+
+// app.use("/hello/guru",(req,res)=>{
+//   res.send('Hai guru!');
+// });
+
+// app.use("/hello",(req,res)=>{
+//   res.send('Hai Naresh!');
+// });
+
+
+// app.use("/test",(req,res)=>{
+//   res.send('Hai Test!');
+// });
 
 // app.use("/",(req,res)=>{
 //   res.send('Hello from Express!');
